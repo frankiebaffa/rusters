c40d4fcbc8d098741734609d7c0e8e16ad4dae09
Frankie Baffa <frankie.baffa@gentryhealthservices.com>
2021-10-06 15:26:02 -0400

    add active column

--- sql/migrations/tbl_sessioncookies/up.sql
+++ sql/migrations/tbl_sessioncookies/up.sql
@@ -3,6 +3,7 @@ create table RustersDb.SessionCookies
 		PK integer not null primary key autoincrement
 	,	Session_PK integer not null
 	,	Name text not null
+	,	Active integer not null default 1
 	,	Value text not null
 	,	Created_DT text not null
 	,	foreign key (Session_PK) references Sessions (PK)
